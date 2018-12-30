12c879a0453d09c198df869da9c3043e2a085708
Brendan Goodenough <brendan@goodenough.nz>
2018-12-31 10:58:33 +1300

    Update meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Brendan Goodenough`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `My personal blog where I speak my mind.`,
     author: `Brendan Goodenough`,
   },
   plugins: [
